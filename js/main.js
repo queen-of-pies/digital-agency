7450e53a286ce6734c8c848ad2d1482c1a8d1945
queen-of-pies <volkova_ta_job@mail.ru>
2021-04-01 00:20:26 +1000

    create: responsive portfolio

--- js/main.js
+++ js/main.js
@@ -1,13 +1,29 @@
 const portfolioSlider = new Swiper(".portfolio-slider", {
   slidesPerView: 3.5,
-
-  spaceBetween: 100,
-
-  // slidesPerGroup: 0.5,
-
   // Navigation arrows
   navigation: {
     nextEl: ".portfolio-slider__button--next",
     prevEl: ".portfolio-slider__button--prev",
+  },
+  // Responsive breakpoints
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    567: {
+      slidesPerView: 1.5,
+    },
+    767: {
+      slidesPerView: 2,
+    },
+    992: {
+      slidesPerView: 2.5,
+    },
+    1100: {
+      slidesPerView: 3,
+    },
+    1300: {
+      slidesPerView: 3.5,
+    },
   },
 });
