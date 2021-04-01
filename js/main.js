13ea9b598a5f3aa62c8f20a5868bf6eebe68db39
queen-of-pies <volkova_ta_job@mail.ru>
2021-04-01 23:54:06 +1000

    fix: portfolio slider

--- js/main.js
+++ js/main.js
@@ -1,32 +1,11 @@
 const portfolioSlider = new Swiper(".portfolio-slider", {
-  slidesPerView: 3.5,
-
+  slidesPerView: "auto",
+  // slidesPerGroup: 3,
   spaceBetween: 30,
   // Navigation arrows
   navigation: {
     nextEl: ".portfolio-slider__button--next",
     prevEl: ".portfolio-slider__button--prev",
-  },
-  // Responsive breakpoints
-  breakpoints: {
-    320: {
-      slidesPerView: 1,
-    },
-    567: {
-      slidesPerView: 1.5,
-    },
-    767: {
-      slidesPerView: 2,
-    },
-    992: {
-      slidesPerView: 2.5,
-    },
-    1100: {
-      slidesPerView: 3,
-    },
-    1300: {
-      slidesPerView: 3.5,
-    },
   },
 });
 const reviewsSlider = new Swiper(".reviews-slider", {
